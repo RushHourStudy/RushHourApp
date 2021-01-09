f20d29b975e8ef4ff588769c49ef8445b56fe5e8
sgtanvi <54735473+sgtanvi@users.noreply.github.com>
2021-01-08 21:52:30 -0800

    -Added Images -Timer has a set button -Timer can now function properly even if the screen is rotated - Problem found in MainActivity.java

--- app/src/main/java/com/example/rushhour/MainActivity.java
+++ app/src/main/java/com/example/rushhour/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -19,14 +21,21 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonStartPause;
     private Button mButtonReset;
 
+    //User input Set button
+    private Button mButtonSet;
+    private EditText mEditTextInput;
+
     //Rewards Button
     private Button mButtonReward;
+
 
     private CountDownTimer mCountDownTimer;
 
     private boolean mTimerRunning;
 
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private long mEndTime;
+
 
 
 
@@ -41,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         mButtonReset = findViewById(R.id.button_reset);
 
         mButtonReward = findViewById(R.id.button_reward); // Initializing Reward Button on 1st activity page
+        mButtonSet = findViewById(R.id.button_set); //Initialzing Set Button on 1st activity page
 
         //Uses method to open rewards page
         mButtonReward.setOnClickListener(new View.OnClickListener() {
@@ -48,93 +58,160 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 openRewardsPage();
             }
+            //method to open rewards page
+            public void openRewardsPage() {
+                Intent intent = new Intent(MainActivity.this, RewardsPage.class);
+                startActivity(intent);
+            }
+
         });
 
 
-        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+        //Uses method to recognize
+        mButtonSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mTimerRunning) {
-                    pauseTimer();
-                } else {
-                    startTimer();
+                String input = mEditTextInput.getText().toString();
+                if (input.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+            }});
 
-            }
-        });
 
-        mButtonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTimer();
-            }
-        });
 
-        updateCountDownText();
-    }
+                mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mTimerRunning) {
+                            pauseTimer();
+                        } else {
+                            startTimer();
+                        }
+                        mButtonReward.setText("Reward");
+                        mButtonReward.setVisibility(View.VISIBLE);
+                    }
+                });
 
-    //method to open rewards page
-    public void openRewardsPage() {
-        Intent intent = new Intent(this, Rewards_Page.class);
-        startActivity(intent);
-    }
+                mButtonReset.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetTimer();
+                    }
+                });
 
-    private void startTimer() {
-        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeftInMillis = millisUntilFinished;
                 updateCountDownText();
+
+
+            }
+
+
+            private void startTimer() {
+                mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        mTimeLeftInMillis = millisUntilFinished;
+                        updateCountDownText();
+                        mButtonReward.setText("Reward");
+                        mButtonReward.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        mTimerRunning = false;
+                        updateButtons();
+                        //Need to call method to collect points
+
+
+                    }
+                }.start();
+                mTimerRunning = true;
+                updateButtons();
+
+
+            }
+
+            private void pauseTimer() {
+                mCountDownTimer.cancel();
+                mTimerRunning = false;
+                updateButtons();
+            }
+
+            private void resetTimer() {
+
+                mTimeLeftInMillis = START_TIME_IN_MILLIS;
+                updateCountDownText();
+                updateButtons();
+
+            }
+
+
+            private void updateCountDownText() {
+                int minuets = (int) mTimeLeftInMillis / 1000 / 60;
+                int seconds = (int) mTimeLeftInMillis / 1000 % 60;
+
+                String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minuets, seconds);
+
+                mTextViewCountDown.setText(timeLeftFormatted);
+            }
+
+            //Makes sure that when rotating device, timer does not change
+            private void updateButtons() {
+                if (mTimerRunning) {
+                    mButtonReset.setVisibility(View.INVISIBLE);
+                    mButtonStartPause.setText("Pause");
+                    mButtonReward.setVisibility(View.INVISIBLE);
+                } else {
+                    mButtonStartPause.setText("Start");
+                    if (mTimeLeftInMillis < 1000) {
+                        mButtonStartPause.setVisibility(View.INVISIBLE);
+                    } else {
+                        mButtonStartPause.setVisibility(View.VISIBLE);
+                    }
+                    if (mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+                        mButtonReset.setVisibility(View.VISIBLE);
+                        mButtonReward.setVisibility(View.VISIBLE);
+                    } else {
+                        mButtonReset.setVisibility(View.INVISIBLE);
+                        mButtonReward.setVisibility(View.INVISIBLE);
+                    }
+                }
+            }
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("millisLeft", mTimeLeftInMillis);
+        outState.putBoolean("timerRunning", mTimerRunning);
+        outState.putLong("endTime", mEndTime);
+    }
+            @Override
+            protected void onSaveInstanceState(Bundle outState) {
+                super.onSaveInstanceState(outState);
+                outState.putLong("millisLeft", mTimeLeftInMillis);
+                outState.putBoolean("timerRunning", mTimerRunning);
+                outState.putLong("endTime", mEndTime);
             }
 
             @Override
-            public void onFinish() {
-                mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
-                mButtonReward.setText("Reward");
-                mButtonReward.setVisibility(View.VISIBLE);
-
+            protected void onRestoreInstanceState(Bundle savedInstanceState) {
+                super.onRestoreInstanceState(savedInstanceState);
+                mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+                mTimerRunning = savedInstanceState.getBoolean("timerRunning");
+                updateCountDownText();
+                updateButtons();
+                    if (mTimerRunning) {
+                        mEndTime = savedInstanceState.getLong("endTime");
+                        mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+                        startTimer();
+                    }
             }
-        }.start();
-        mTimerRunning = true;
-        mButtonStartPause.setText("pause");
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonReward.setText("Reward");
-        mButtonReward.setVisibility(View.INVISIBLE);
 
 
-    }
 
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning=false;
-        mButtonStartPause.setText("Start");
-        mButtonReset.setVisibility(View.VISIBLE);
-        mButtonReward.setText("Reward");
-        mButtonReward.setVisibility(View.VISIBLE);
-    }
-
-    private void resetTimer() {
-
-        mTimeLeftInMillis=START_TIME_IN_MILLIS;
-        updateCountDownText();
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
-        mButtonReward.setVisibility(View.VISIBLE);
-
-    }
-
-
-    private void updateCountDownText() {
-    int minuets = (int) mTimeLeftInMillis / 1000 / 60;
-    int seconds = (int) mTimeLeftInMillis / 1000 % 60;
-
-    String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minuets, seconds);
-
-        mTextViewCountDown.setText(timeLeftFormatted);
-    }
 }
 
 
+/* https://www.w3adda.com/android-tutorial/android-intents   - will help with the data access */
--- app/src/main/java/com/example/rushhour/Rewards_Page.java
+++ app/src/main/java/com/example/rushhour/Rewards_Page.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class Rewards_Page extends AppCompatActivity {
+class RewardsPage extends AppCompatActivity {
     private String name;
-    private int points;
+    private int points = 0;
 
+    //mutator to collect points
     public void collectPoints() {
         points = points + 1;
 
     }
 
 
+    //accessor returns points
     public int getPoints() {
         return points;
     }
